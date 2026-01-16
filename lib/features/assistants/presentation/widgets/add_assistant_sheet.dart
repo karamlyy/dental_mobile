cb3ffa790f01ff0d1b4b15d912c6224579f63956
Karam Afandi <karam_afandi@yahoo.com>
2026-01-16 09:18:11 +0400

    fix: add assistants sheet open keyboard fix

--- lib/features/assistants/presentation/widgets/add_assistant_sheet.dart
+++ lib/features/assistants/presentation/widgets/add_assistant_sheet.dart
@@ -80,10 +80,11 @@ class _AddAssistantSheetState extends State<AddAssistantSheet> {
               ),
               child: Form(
                 key: _formKey,
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: [
+                child: SingleChildScrollView(
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
                     /// 🔹 Drag handle
                     Center(
                       child: Container(
@@ -211,7 +212,8 @@ class _AddAssistantSheetState extends State<AddAssistantSheet> {
                       icon: Icons.add,
                     ),
                     const SizedBox(height: 16),
-                  ],
+                    ],
+                  ),
                 ),
               ),
             ),
